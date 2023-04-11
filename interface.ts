536daeaf8e241a10442d9f8ad0d092d295e1db92
tk <90978402+totolkat@users.noreply.github.com>
2023-04-11 09:37:49 +0100

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -1,1 +1,12 @@
+interface User {
+  username: string,
+  age: number
+}
 
+function user(register: User) {
+  console.log(register.username, register.age)
+}
+  
+
+const newUser = { username: "totolkat", age: 14 } 
+console.log(newUser)
